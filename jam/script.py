7478378a027b21dbeec3ee6aa7175fe60830e60f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2019-12-06 21:13:59 +0100

    feat(script): add first draft to run internal python source code
    + add error handler
    
    + add unit tests
    + add failing master test to ensure expected final behavior

---
+++ jam/script.py
@@ -0,0 +1,128 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""todo
+====
+
+example
+-------
+
+.. code-block:: python
+
+    page_0.text_0 = 'hello world'
+
+    page_0.text_5.size = 14
+    page_0.text_5.font = 'Verdana'
+
+    page_0.text_3.bold = True
+
+    del page_0.text_1
+"""
+import dataclasses
+import runpy
+
+import PyPDF2
+import utila
+
+import jam
+
+
+@dataclasses.dataclass
+class Status:
+    ready: bool = False
+    error: bool = False
+
+
+def run(script: str, document: str) -> int:
+    doc = Document(document)
+    status = Status()
+    environment = {'__status': status}
+    environment.update(doc.pages())
+
+    script = scriptfile(script)
+    try:
+        result = runpy.run_path(
+            script,
+            init_globals=environment,
+            run_name=None,
+        )
+    except SyntaxError as error:
+        utila.error(error)
+        return utila.FAILURE
+    return utila.SUCCESS if status.ready else utila.FAILURE
+
+
+class Document:
+
+    def __init__(self, source: str):
+        self.loaded = PyPDF2.PdfFileReader(stream=open(source, mode='rb'))
+        self.update_accessor()
+
+    def update_accessor(self):
+        print('update accessor')
+        pass
+
+    def pages(self):
+        return {
+            f'page_{number}': 'hello'
+            for number in range(self.loaded.getNumPages())
+        }
+
+    # def __setattr__(self, instance, value):
+    #     try:
+    #         print(instance)
+    #         print(value)
+    #         print()
+    #         self.data[instance] = value
+    #         assert any([
+    #             instance.startswith('page_'),
+    #             instance.startswith('final'),
+    #         ]), instance
+    #     except AttributeError:
+    #         super().__setattr__('data', {})
+
+    # def __getattr__(self, name):
+    #     datadict = super().__getattribute__('data')
+    #     return datadict[name]
+
+
+class Text:
+
+    def __init__(self):
+        pass
+
+
+def scriptfile(path: str) -> str:
+    """Create scriptfile for exection and surround code with error
+    handler.
+
+    Args:
+        path(str): path to source code file
+    Returns:
+        Path to generated source code file which contains error handler.
+    """
+    loaded = utila.file_read(path)
+    # ensure indent
+    loaded = [f'    {item}' for item in loaded.splitlines()]
+    loaded = utila.NEWLINE.join(loaded)
+    with_final = (ERROR_HANDLER % loaded)
+
+    filepath = utila.tmpfile(jam.ROOT)
+    utila.file_replace(filepath, with_final)
+    return filepath
+
+
+ERROR_HANDLER = """\
+try:
+    %s
+except Exception as error:
+    print(error)
+    __status.error = True
+else:
+    __status.ready = True
+"""
---
+++ tests/test_script.py
@@ -0,0 +1,64 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import pytest
+import utila
+
+import jam.script
+import tests.resources
+
+EXAMPLE = """
+print('hello')
+page_0 = 5
+"""
+
+SYNTAX_ERROR = """
+print('hello') 4
+"""
+
+RUNTIME_ERROR = """
+assert 0
+"""
+
+MASTER = """
+page_0.text_0 = 'hello world'
+
+page_0.text_5.size = 14
+page_0.text_5.font = 'Verdana'
+
+page_0.text_3.bold = True
+
+del page_0.text_1
+"""
+
+
+@pytest.mark.xfail(reason='not fully implemented')
+def test_script_master(testdir):
+    root = str(testdir)
+    script = os.path.join(root, 'master.py')
+    utila.file_create(script, MASTER)
+
+    completed = jam.script.run(script, document=tests.resources.MASTER_72PAGES)
+    assert completed == utila.SUCCESS
+
+
+@pytest.mark.parametrize('code, expected', [
+    pytest.param(EXAMPLE, utila.SUCCESS, id='simple'),
+    pytest.param(SYNTAX_ERROR, utila.FAILURE, id='syntaxerror'),
+    pytest.param(RUNTIME_ERROR, utila.FAILURE, id='runtimeerror'),
+])
+def test_script_execution(code, expected, testdir):
+    root = str(testdir)
+    script = os.path.join(root, 'source.py')
+    utila.file_create(script, code)
+
+    completed = jam.script.run(script, document=tests.resources.MASTER_72PAGES)
+    assert completed == expected
