c8f31db8a1b09a991a6a5448de63739c8fd52c4c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-26 13:34:06 +0100

    0.1.11 automatically generated release

--- jam/__init__.py
+++ jam/__init__.py
@@ -8,7 +8,7 @@
 #==============================================================================
 import os
 
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PACKAGE = 'jam'
