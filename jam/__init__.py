31df4b500e4959006e809bf4788e57c16ac69547
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 00:37:19 +0200

    0.2.6 automatically generated release

--- jam/__init__.py
+++ jam/__init__.py
@@ -18,7 +18,7 @@ from jam.pdf import write
 from jam.script import run
 from jam.script import scriptfile
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PACKAGE = 'jam'
