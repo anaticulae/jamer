8fd3351897fac9a912e786349ae5476a4051affc
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-03 11:29:55 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,49 +8,18 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'jam/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'jam',
+    'jam.cli',
+    'pdfsmall',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'jam = jam.cli:main',
+    'pdfcat = jam.cat:main',
+    'pdfsmall = pdfsmall.cli:main',
+])
 
 if __name__ == "__main__":
-    setuptools.setup(
-        author='Helmut Konrad Schewe',
-        author_email='info@checkitweg.de',
-        description='modify test data',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='jam',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/jam',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'jam',
-            'jam.cli',
-            'pdfsmall',
-        ],
-        entry_points={
-            'console_scripts': [
-                'jam = jam.cli:main',
-                'pdfcat = jam.cat:main',
-                'pdfsmall = pdfsmall.cli:main',
-            ],
-        },
-    )
+    utila.install(__file__)
